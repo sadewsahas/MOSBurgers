24ac7239645ae93c9c26193632cbd9d65f9d73b0
sadewsahas <sadev123sahasakara@gmail.com>
2024-07-27 19:32:05 +0530

    first commit

---
+++ addtocart.js
@@ -0,0 +1,336 @@
+const product = [
+    {
+        id: 0,
+        image: 'img/BurgerImg.png',
+        title: 'Classic Burger (Regular)',
+        price: 750,
+    },
+    {
+        id: 1,
+        image: 'img/BurgerImg.png',
+        title: 'Classic Burger (Large)',
+        price: 1500,
+    },
+    {
+        id: 2,
+        image: 'img/BurgerImg.png',
+        title: 'Turkey Burger',
+        price: 1600,
+    },
+    {
+        id: 3,
+        image: 'img/BurgerImg.png',
+        title: 'Chicken Burger (Regular)',
+        price: 800,
+    },
+    {
+        id: 4,
+        image: 'img/BurgerImg.png',
+        title: 'Chicken Burger (Large)',
+        price: 1000,
+    },
+    {
+        id: 5,
+        image: 'img/BurgerImg.png',
+        title: 'Bacon Burger',
+        price: 650,
+    },
+    {
+        id: 6,
+        image: 'img/BurgerImg.png',
+        title: 'Shawarma Burger',
+        price: 800,
+    },
+    {
+        id: 7,
+        image: 'img/BurgerImg.png',
+        title: 'Olive Burger',
+        price: 1800,
+    },
+    {
+        id: 8,
+        image: 'img/BurgerImg.png',
+        title: 'Double-Cheese Burger',
+        price: 1250,
+    },
+    {
+        id: 9,
+        image: 'img/BurgerImg.png',
+        title: 'Cheese Burger (Large)',
+        price: 1000,
+    },
+    {
+        id: 10,
+        image: 'img/BurgerImg.png',
+        title: 'Cheese Burger (Regular)',
+        price: 600,
+    },
+    {
+        id: 11,
+        image: 'img/BurgerImg.png',
+        title: 'Crispy Chicken Burger (Large)',
+        price: 1600,
+    },
+    {
+        id: 12,
+        image: 'img/BurgerImg.png',
+        title: 'Crispy Chicken Burger (Regular)',
+        price: 1600,
+    },
+    {
+        id: 13,
+        image: 'img/BurgerImg.png',
+        title: 'Paneer Burger',
+        price: 900,
+    },
+    {
+        id: 14,
+        image: 'img/SubsImg.png',
+        title: 'Crispy Chicken Sub (Large)',
+        price: 2000,
+    },
+    {
+        id: 15,
+        image: 'img/SubsImg.png',
+        title: 'Crispy Chicken Sub (Regular)',
+        price: 1500,
+    },
+    {
+        id: 16,
+        image: 'img/SubsImg.png',
+        title: 'Chicken Sub (Large)',
+        price: 1800,
+    },
+    {
+        id: 17,
+        image: 'img/SubsImg.png',
+        title: 'Chicken Sub (Regular)',
+        price: 1400,
+    },
+    {
+        id: 18,
+        image: 'img/SubsImg.png',
+        title: 'Grinder Sub',
+        price: 2300,
+    },
+    {
+        id: 19,
+        image: 'img/SubsImg.png',
+        title: 'Cheese Sub',
+        price: 2200,
+    },
+    {
+        id: 20,
+        image: 'img/SubsImg.png',
+        title: 'Double Cheese n Chicken Sub',
+        price: 1900,
+    },
+    {
+        id: 21,
+        image: 'img/SubsImg.png',
+        title: 'Special Horgie Sub',
+        price: 2800,
+    },
+    {
+        id: 22,
+        image: 'img/SubsImg.png',
+        title: 'MOS Special Sub',
+        price: 3000,
+    },
+    {
+        id: 23,
+        image: 'img/FImg.png',
+        title: 'Steak Fries (Large)',
+        price: 1200,
+    },
+    {
+        id: 24,
+        image: 'img/FImg.png',
+        title: 'Steak Fries (Regular)',
+        price: 600,
+    },
+    {
+        id: 25,
+        image: 'img/FImg.png',
+        title: 'French Fries (Large)',
+        price: 800,
+    },
+    {
+        id: 26,
+        image: 'img/FImg.png',
+        title: 'French Fries (Medium)',
+        price: 650,
+    },
+    {
+        id: 27,
+        image: 'img/FImg.png',
+        title: 'French Fries (Small)',
+        price: 450,
+    },
+    {
+        id: 28,
+        image: 'img/FImg.png',
+        title: 'Sweet Potato Fries (Large)',
+        price: 600,
+    },
+    {
+        id: 29,
+        image: 'img/PasImg.png',
+        title: 'Chicken n Cheese Pasta',
+        price: 1600,
+    },
+    {
+        id: 30,
+        image: 'img/PasImg.png',
+        title: 'Chciken Penne Pasta',
+        price: 1700,
+    },
+    {
+        id: 31,
+        image: 'img/PasImg.png',
+        title: 'Ground Turkey Pasta Bake',
+        price: 2900,
+    },
+    {
+        id: 32,
+        image: 'img/PasImg.png',
+        title: 'Creamy Shrimp Pasta',
+        price: 2000,
+    },
+    {
+        id: 33,
+        image: 'img/PasImg.png',
+        title: 'Lemon Butter Pasta',
+        price: 1950,
+    },
+    {
+        id: 34,
+        image: 'img/PasImg.png',
+        title: 'Tagliatelle Pasta',
+        price: 2400,
+    },
+    {
+        id: 35,
+        image: 'img/PasImg.png',
+        title: 'Baked Ravioli',
+        price: 2000,
+    },
+    {
+        id: 36,
+        image: 'img/ChImg.png',
+        title: 'Fried Chicken (Small)',
+        price: 1200,
+    },
+    {
+        id: 37,
+        image: 'img/ChImg.png',
+        title: 'Fried Chicken (Regular)',
+        price: 2300,
+    },
+    {
+        id: 38,
+        image: 'img/ChImg.png',
+        title: 'Fried Chicken (Large)',
+        price: 3100,
+    },
+    {
+        id: 39,
+        image: 'img/ChImg.png',
+        title: 'Hot Wings (Large)',
+        price: 2400,
+    },
+    {
+        id: 40,
+        image: 'img/ChImg.png',
+        title: 'Devilled Chicken (Large)',
+        price: 900,
+    },
+    {
+        id: 41,
+        image: 'img/ChImg.png',
+        title: 'BBQ Chicken (Regular)',
+        price: 2100,
+    },
+    {
+        id: 42,
+        image: 'img/Pes.png',
+        title: 'Pepsi (330 ML)',
+        price: 990,
+    },
+    {
+        id: 43,
+        image: 'img/Coc.png',
+        title: 'Coca-Cola (330 ML)',
+        price: 1230,
+    },
+    {
+        id: 44,
+        image: 'img/Sprite.png',
+        title: 'Sprite (330 ML)',
+        price: 1500,
+    },
+    {
+        id: 45,
+        image: 'img/Mirnd.png',
+        title: 'Mirinda (330 ML)',
+        price: 850,
+    }
+];
+const categories = [...new Set(product.map((item) => { return item }))]
+let i = 0;
+document.getElementById('root').innerHTML = categories.map((item) => {
+    var { image, title, price } = item;
+    return (
+        `<div class='box'>
+           <div class='img-box'>
+               <img class='images' src=${image}></img>
+           </div>
+       <div class='bottom'>
+       <p>${title}</p>
+       <h2>Rs. ${price}.00</h2>` +
+        "<button onclick='addtocart(" + (i++) + ")'>Add to cart</button>" +
+        `</div>
+       </div>`
+    )
+}).join('')
+
+var cart = [];
+
+function addtocart(a) {
+    cart.push({ ...categories[a] });
+    displaycart();
+}
+function delElement(a) {
+    cart.splice(a, 1);
+    displaycart();
+}
+
+function displaycart() {
+    let j = 0, total = 0;
+    document.getElementById("count").innerHTML = cart.length;
+    if (cart.length == 0) {
+        document.getElementById('cartItem').innerHTML = "Your cart is empty";
+        document.getElementById("total").innerHTML = "$" + 0 + ".00";
+    }
+    else {
+        document.getElementById("cartItem").innerHTML = cart.map((items) => {
+            var { image, title, price } = items;
+            total = total + price;
+            document.getElementById("total").innerHTML = "Rs. " + total + ".00";
+            return (
+                `<div class='cart-item'>
+               <div class='row-img'>
+                   <img class='rowimg' src=${image}>
+               </div>
+               <p style='font-size:12px;'>${title}</p>
+               <h2 style='font-size: 15px;'>Rs. ${price}.00</h2>` +
+                "<i class='fa-solid fa-trash' onclick='delElement(" + (j++) + ")'></i></div>"
+            );
+        }).join('');
+    }
+
+
+}
+
+
+
